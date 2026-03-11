184227e47903d684abebbe5a7de2499b3a610957
Jamily Alecrim <jmysvt@gmail.com>
2026-03-11 11:44:46 -0300

    git commit -m "ABCGJL-184 Criação da pagina de login" feat: Criação do LoginView.kt chore: atualização do MainActivity.kt

--- app/src/main/java/com/senai/abcgjl_smartcuisine_mobile/MainActivity.kt
+++ app/src/main/java/com/senai/abcgjl_smartcuisine_mobile/MainActivity.kt
@@ -4,44 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.senai.abcgjl_smartcuisine_mobile.ui.theme.LoginView
 import com.senai.abcgjl_smartcuisine_mobile.ui.theme.SmartCuisineTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SmartCuisineTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                Box(modifier = Modifier.fillMaxSize()) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.aquarelafundo),
+                        contentDescription = "aquarela do fundo",
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop)
+
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = Color.Transparent
+                    ) {
+                        LoginView(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(horizontal = 32.dp)
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartCuisineTheme {
-        Greeting("Android")
     }
 }
