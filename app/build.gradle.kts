b0fc27261f1b0e6344bead12bd75698ef4efb289
Jamily Alecrim <jmysvt@gmail.com>
2026-04-27 10:25:18 -0300

    ABCGJL-214 Criar tela de visualização do “processo” do sistema (mínimo comum)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.runtime)
     implementation(libs.firebase.appdistribution.gradle)
+    implementation(libs.firebase.crashlytics.buildtools)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
